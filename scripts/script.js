8e37d24f611681c8b9696d82c234f7c69c1f8066
Simon Olsson <scmolsson@pm.me>
2025-02-03 13:10:18 +0100

    created a function to pick 10 random pokemons

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,41 @@
 const log = (msg) => console.log(msg);
 
 // I denna fil skriver ni all er kod
+
+randomPokemons();
+
+function randomPokemons () {
+    const allPokemons = [];    
+
+    for (let i = 1; i < 152; i++){
+        let pokemon = i  
+        allPokemons.push(pokemon);
+    }
+
+    for (let i = 1; i < 10 + 1; i++) {
+        let random = i;
+        random = Math.floor(Math.random() * allPokemons.length + 1);
+        let pokemon = random.toString().padStart(3,"0");
+        oGameData.pokemonNumbers.push(`assets/pokemons/${pokemon}.png`);
+    }
+}
+
+console.log(oGameData.pokemonNumbers);
+
+// let mainRef = document.querySelector('main');
+// let pokemonRef = oGameData.pokemonNumbers.forEach(src => {
+//     let img = document.createElement('img');
+//     img.src = src;
+//     mainRef.appendChild(img)
+// })
+
+
+
+
+
+
+
+
+
+
+
