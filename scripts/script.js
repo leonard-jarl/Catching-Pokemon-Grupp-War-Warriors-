0b240fd448b0686cd798ad011afe4de220966724
Dennis Collins <donncha.ocoileain@hotmail.com>
2025-02-03 11:48:15 +0100

    Added startTimer function to script.

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,15 @@
 const log = (msg) => console.log(msg);
 
 // I denna fil skriver ni all er kod
+
+// Event listener som ska anropa startTimer när Start-knappen clickas.
+document.getElementById("startButton").addEventListener("click", function () {
+  startTimer();
+});
+
+// Function för att starta timern när spelet börjar.
+function startTimer() {
+  oGameData.startTimeInMilliseconds();
+  console.log("Game timer has started.");
+  console.log("Start time (ms):", oGameData.startTime);
+}
