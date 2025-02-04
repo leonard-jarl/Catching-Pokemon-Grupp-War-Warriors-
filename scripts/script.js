74967330ae877e6c392913c9b16e2fa4fb9a77c0
Nabeel Mansour <mikaeel1@hotmail.com>
2025-02-04 09:54:47 +0100

    validator fixed

--- scripts/script.js
+++ scripts/script.js
@@ -31,10 +31,10 @@ function validateForm() {
   const gender = document.querySelector("#genderInput").value;
 
   try {
-    if (name.length < 5 || name.length >= 10) {
+    if (name.length < 5 || name.length > 10) {
       throw new Error("namn måste vara mellan 5 och 10 tecken långt");
     }
-    if (age < 9 || age > 15) {
+    if (age < 10 || age > 15) {
       throw new Error("Ålder måste vara mellan 10 och 15 år gammal");
     }
     if (gender !== "Boy" && gender !== "Girl") {
