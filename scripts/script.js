d0708d23ed2dc03f9980cef7a2452571e7299d84
LeonardUlander <3kdyhfed24.ulanle@folkuniversitetet.nu>
2025-02-04 14:08:43 +0100

    Fixed faulty connection between js files

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,8 @@
 "use strict";
 
-let oGameData = {};
-
 window.addEventListener("load", () => {
-  init();
   prepGame();
 });
-
-function init() {
-  oGameData.pokemonNumbers = [];
-  oGameData.nmbrOfCaughtPokemons = 0;
-  oGameData.startTime = 0;
-  oGameData.endTime = 0;
-  oGameData.timerId = null;
-  oGameData.nmbrOfSeconds = 0;
-  oGameData.trainerName = "";
-  oGameData.trainerAge = 0;
-  oGameData.trainerGender = "";
-}
 
 function prepGame() {
   let startButton = document.getElementById("startButton");
